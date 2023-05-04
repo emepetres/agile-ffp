f799feb1975b85c748beab7dc63ff98bd9900ee1
Javier Carnero <jcarnero@plainconcepts.com>
2023-05-04 11:56:28 +0200

    test reproducing depencencies bug

--- tests/gantt/task_should.py
+++ tests/gantt/task_should.py
@@ -15,6 +15,11 @@ def task2():
 
 
 @pytest.fixture
+def task3():
+    return Task("sample_task2", {"team3": 10})
+
+
+@pytest.fixture
 def wrong_task():
     return Task("wrong_task", {"team1": 10, "team3": 13})
 
@@ -24,6 +29,7 @@ def capacity():
     return {
         "team1": CapacityTeam("team1", 2, date(2023, 1, 1)),
         "team2": CapacityTeam("team2", 3, date(2023, 1, 1)),
+        "team3": CapacityTeam("team2", 2, date(2023, 1, 1)),
     }
 
 
@@ -41,3 +47,14 @@ def assert_duration_according_to_capacity(task1, task2, capacity):
     assert task2.init == date(2023, 1, 9)
     assert task2.end == date(2023, 1, 16)
     assert task2.days == 6
+
+
+def assert_force_init_date(task1, task3, capacity):
+    task1.assign_capacity(capacity)
+    assert task1.init == date(2023, 1, 2)
+    assert task1.end == date(2023, 1, 9)
+    assert task1.days == 5
+    task3.assign_capacity(capacity, date(2023, 1, 10))
+    assert task2.init == date(2023, 1, 10)
+    assert task2.end == date(2023, 1, 16)
+    assert task2.days == 5
