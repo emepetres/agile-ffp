5e77b0a5e34b2411980716b5c1f8d6c60a147d6d
Javier Carnero <jcarnero@plainconcepts.com>
2023-05-10 14:20:54 +0200

    support management on milestones

--- src/agileffp/milestone/estimation.py
+++ src/agileffp/milestone/estimation.py
@@ -1,8 +1,15 @@
 class EstimatedEpic:
-    def __init__(self, name: str, totals: dict[str, int], tasks: list):
+    def __init__(
+        self,
+        name: str,
+        totals: dict[str, int],
+        tasks: list,
+        management_team: str = "management",
+    ):
         self.name = name
         self.totals = totals
         self.tasks = [EstimatedTask(name, totals, **t) for t in tasks]
+        self.management_team = management_team
         self._compute_tasks_effort()
 
     def _compute_tasks_effort(self):
@@ -16,6 +23,17 @@ class EstimatedEpic:
                 t.computed_effort[k] = (self.totals.get(k, 0) * v) / raw_totals.get(
                     k, 0
                 )
+
+        total_except_management = 0
+        for k, v in self.totals.items():
+            if k != self.management_team:
+                total_except_management += v
+
+        for t in self.tasks:
+            raw_totals = sum(t.computed_effort.values())
+            t.computed_effort[self.management_team] = (
+                raw_totals * self.totals.get(self.management_team, 0)
+            ) / total_except_management
 
 
 class EstimatedTask:
