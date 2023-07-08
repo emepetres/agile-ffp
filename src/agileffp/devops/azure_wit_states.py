f3fdfc287e0c798a008f877ed9d08b78b65ea431
Javier Carnero <jcarnero@plainconcepts.com>
2023-07-08 21:32:49 +0200

    parsing wit state updates

--- src/agileffp/devops/azure_wit_states.py
+++ src/agileffp/devops/azure_wit_states.py
@@ -1,6 +1,10 @@
 import pandas as pd
+from dateutil import parser
 from agileffp.devops import config
-from agileffp.devops.azure_query_results import get_work_items_from_query
+from agileffp.devops.azure_query_results import (
+    get_work_item_updates,
+    get_work_items_from_query,
+)
 
 
 def compute_states_from_query() -> pd.DataFrame:
@@ -12,5 +16,41 @@ def compute_states_from_query() -> pd.DataFrame:
 
 
 def compute_work_items_states(data: dict) -> pd.DataFrame:
-    df = pd.DataFrame(columns=[wit["name"] for wit in data["columns"]])
+    raise NotImplementedError()
+
+
+def _get_wit_ids_list(data: dict) -> list:
+    """Returns a list of work items ids from a query result."""
+    return [wit["id"] for wit in data["workItems"]]
+
+
+def get_wit_state_updates(data: dict) -> pd.DataFrame:
+    """Writes work items state updates in a time-series format table."""
+    df = pd.DataFrame(columns=["id", "date", "state"])
+    wit_list = _get_wit_ids_list(data)
+    for wit_id in wit_list:
+        wit_updates = get_work_item_updates(
+            config.PAT, config.SERVER, config.ORGANIZATION, config.PROJECT, wit_id
+        )
+        for update in wit_updates["value"]:
+            update_data = parse_state_update(update)
+            if update_data:
+                df = df.append(update_data, ignore_index=True)
     return df
+
+
+def parse_state_update(update: dict) -> list:
+    """Parses a work item state update."""
+    if "fields" in update and "System.State" in update["fields"]:
+        return [
+            update["workItemId"],
+            parser.parse(update["revisedDate"]),
+            update["fields"]["System.State"]["newValue"],
+        ]
+    else:
+        return []
+
+
+def get_wit_metadata(data: dict) -> pd.DataFrame:
+    """Transforms work items metadata into a pandas DataFrame."""
+    raise NotImplementedError()
--- tests/devops/azure_wit_states_should.py
+++ tests/devops/azure_wit_states_should.py
@@ -1,30 +1,252 @@
 import pytest
-from agileffp.devops.azure_wit_states import compute_work_items_states
+from dateutil import parser
+
+from agileffp.devops.azure_wit_states import get_wit_state_updates, parse_state_update
+
 
 @pytest.fixture
-def data():
+def empty_query_result():
     return {
         "queryType": "flat",
         "queryResultType": "workItem",
         "asOf": "2021-03-01T09:00:00Z",
         "columns": [
-            {"name": "Id", "referenceName": "System.Id", "url": "https://dev.azure.com/organization/project/_apis/wit/fields/System.Id"},
-            {"name": "Title", "referenceName": "System.Title", "url": "https://dev.azure.com/organization/project/_apis/wit/fields/System.Title"},
-            {"name": "State", "referenceName": "System.State", "url": "https://dev.azure.com/organization/project/_apis/wit/fields/System.State"},
-            {"name": "Work Item Type", "referenceName": "System.WorkItemType", "url": "https://dev.azure.com/organization/project/_apis/wit/fields/System.WorkItemType"},
+            {
+                "name": "Id",
+                "referenceName": "System.Id",
+                "url": "https://dev.azure.com/organization/project/_apis/wit/fields/System.Id",
+            },
+            {
+                "name": "Title",
+                "referenceName": "System.Title",
+                "url": "https://dev.azure.com/organization/project/_apis/wit/fields/System.Title",
+            },
+            {
+                "name": "State",
+                "referenceName": "System.State",
+                "url": "https://dev.azure.com/organization/project/_apis/wit/fields/System.State",
+            },
+            {
+                "name": "Work Item Type",
+                "referenceName": "System.WorkItemType",
+                "url": "https://dev.azure.com/organization/project/_apis/wit/fields/System.WorkItemType",
+            },
         ],
-        "workItems": [
-            {"id": "123456", "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123456"},
-            {"id": "123457", "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123457"},
-            {"id": "123458", "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123458"},
-            {"id": "123459", "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123459"},
-            {"id": "123460", "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123460"},
-            {"id": "123461", "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123461"},
-        ]
+        "workItems": [],
     }
 
-def assert_columns_are_parsed(data):
-    df = compute_work_items_states(data)
+
+@pytest.fixture
+def query_result(empty_query_result):
+    empty_query_result["workItems"] = [
+        {
+            "id": "123456",
+            "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123456",
+        },
+        {
+            "id": "123457",
+            "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123457",
+        },
+        {
+            "id": "123458",
+            "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123458",
+        },
+        {
+            "id": "123459",
+            "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123459",
+        },
+        {
+            "id": "123460",
+            "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123460",
+        },
+        {
+            "id": "123461",
+            "url": "https://dev.azure.com/organization/project/_apis/wit/workItems/123461",
+        },
+    ]
+    return empty_query_result
+
+
+@pytest.fixture
+def update():
+    return {
+        "id": 1,
+        "workItemId": 168975,
+        "rev": 1,
+        "revisedBy": {
+            "id": "87270860-c2d5-4d53-a77a-8a15e1fb4680",
+            "name": "Antonio Ávila Membrives <aavila@plainconcepts.com>",
+            "displayName": "Antonio Ávila Membrives",
+            "url": "https://spsprodeus24.vssps.visualstudio.com/A03fda1b0-beae-4349-a672-45e912480c5d/_apis/Identities/87270860-c2d5-4d53-a77a-8a15e1fb4680",
+            "_links": {
+                "avatar": {
+                    "href": "https://dev.azure.com/plainconcepts/_apis/GraphProfile/MemberAvatars/aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx"
+                }
+            },
+            "uniqueName": "aavila@plainconcepts.com",
+            "imageUrl": "https://dev.azure.com/plainconcepts/_apis/GraphProfile/MemberAvatars/aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx",
+            "descriptor": "aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx",
+        },
+        "revisedDate": "2023-06-13T11:57:06.947Z",
+        "fields": {
+            "System.Id": {"newValue": 168975},
+            "System.AreaId": {"newValue": 141633},
+            "System.NodeName": {"newValue": "SecretAligner.CirugiasGuiadas"},
+            "System.AreaLevel1": {"newValue": "SecretAligner.CirugiasGuiadas"},
+            "System.Rev": {"newValue": 1},
+            "System.AuthorizedDate": {"newValue": "2023-06-13T11:54:07.583Z"},
+            "System.RevisedDate": {"newValue": "2023-06-13T11:57:06.947Z"},
+            "System.IterationId": {"newValue": 142694},
+            "System.IterationLevel1": {"newValue": "SecretAligner.CirugiasGuiadas"},
+            "System.IterationLevel2": {"newValue": "Evergine"},
+            "System.IterationLevel3": {"newValue": "Sprint 12"},
+            "System.WorkItemType": {"newValue": "Product Backlog Item"},
+            "System.State": {"newValue": "New"},
+            "System.Reason": {"newValue": "New backlog item"},
+            "System.AssignedTo": {},
+            "System.CreatedDate": {"newValue": "2023-06-13T11:54:07.583Z"},
+            "System.CreatedBy": {
+                "newValue": {
+                    "displayName": "Antonio Ávila Membrives",
+                    "url": "https://spsprodeus24.vssps.visualstudio.com/A03fda1b0-beae-4349-a672-45e912480c5d/_apis/Identities/87270860-c2d5-4d53-a77a-8a15e1fb4680",
+                    "_links": {
+                        "avatar": {
+                            "href": "https://dev.azure.com/plainconcepts/_apis/GraphProfile/MemberAvatars/aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx"
+                        }
+                    },
+                    "id": "87270860-c2d5-4d53-a77a-8a15e1fb4680",
+                    "uniqueName": "aavila@plainconcepts.com",
+                    "imageUrl": "https://dev.azure.com/plainconcepts/_apis/GraphProfile/MemberAvatars/aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx",
+                    "descriptor": "aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx",
+                }
+            },
+            "System.ChangedDate": {"newValue": "2023-06-13T11:54:07.583Z"},
+            "System.ChangedBy": {
+                "newValue": {
+                    "displayName": "Antonio Ávila Membrives",
+                    "url": "https://spsprodeus24.vssps.visualstudio.com/A03fda1b0-beae-4349-a672-45e912480c5d/_apis/Identities/87270860-c2d5-4d53-a77a-8a15e1fb4680",
+                    "_links": {
+                        "avatar": {
+                            "href": "https://dev.azure.com/plainconcepts/_apis/GraphProfile/MemberAvatars/aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx"
+                        }
+                    },
+                    "id": "87270860-c2d5-4d53-a77a-8a15e1fb4680",
+                    "uniqueName": "aavila@plainconcepts.com",
+                    "imageUrl": "https://dev.azure.com/plainconcepts/_apis/GraphProfile/MemberAvatars/aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx",
+                    "descriptor": "aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx",
+                }
+            },
+            "System.AuthorizedAs": {
+                "newValue": {
+                    "displayName": "Antonio Ávila Membrives",
+                    "url": "https://spsprodeus24.vssps.visualstudio.com/A03fda1b0-beae-4349-a672-45e912480c5d/_apis/Identities/87270860-c2d5-4d53-a77a-8a15e1fb4680",
+                    "_links": {
+                        "avatar": {
+                            "href": "https://dev.azure.com/plainconcepts/_apis/GraphProfile/MemberAvatars/aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx"
+                        }
+                    },
+                    "id": "87270860-c2d5-4d53-a77a-8a15e1fb4680",
+                    "uniqueName": "aavila@plainconcepts.com",
+                    "imageUrl": "https://dev.azure.com/plainconcepts/_apis/GraphProfile/MemberAvatars/aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx",
+                    "descriptor": "aad.Njc0MThlZTMtZWY3MS03NDA5LTk2MzctNWQ1YjlkNTRiN2Mx",
+                }
+            },
+            "System.PersonId": {"newValue": 21257114},
+            "System.Watermark": {"newValue": 1011294},
+            "System.IsDeleted": {"newValue": False},
+            "System.CommentCount": {"newValue": 1},
+            "System.TeamProject": {"newValue": "SecretAligner.CirugiasGuiadas"},
+            "System.AreaPath": {"newValue": "SecretAligner.CirugiasGuiadas"},
+            "System.IterationPath": {
+                "newValue": "SecretAligner.CirugiasGuiadas\\Evergine\\Sprint 12"
+            },
+            "System.BoardColumnDone": {"newValue": False},
+            "Microsoft.VSTS.Common.Priority": {"newValue": 2},
+            "Microsoft.VSTS.Common.ValueArea": {"newValue": "Business"},
+            "WEF_074AE9F3108D4C65993E6BEEA1D86824_System.ExtensionMarker": {
+                "newValue": True
+            },
+            "WEF_074AE9F3108D4C65993E6BEEA1D86824_Kanban.Column.Done": {
+                "newValue": False
+            },
+            "WEF_F10BD31DDF194FA7B15351E3F028EBD9_Kanban.Column.Done": {
+                "newValue": False
+            },
+            "WEF_22C2BBA295614D779A28B8CD0DA94944_Kanban.Column": {"newValue": "New"},
+            "WEF_22C2BBA295614D779A28B8CD0DA94944_Kanban.Column.Done": {
+                "newValue": False
+            },
+            "WEF_D96794364361494ABA00132745C4DEB4_Kanban.Column": {"newValue": "New"},
+            "WEF_D96794364361494ABA00132745C4DEB4_Kanban.Column.Done": {
+                "newValue": False
+            },
+            "WEF_27D37A568BC84A79804B57CD0E4B83D1_Kanban.Column": {"newValue": "New"},
+            "WEF_27D37A568BC84A79804B57CD0E4B83D1_Kanban.Column.Done": {
+                "newValue": False
+            },
+            "System.History": {
+                "newValue": 'Copied from <a href="x-mvwit:workitem/168889">Product Backlog Item 168889</a>'
+            },
+            "System.BoardColumn": {"newValue": "New"},
+            "Microsoft.VSTS.Common.StateChangeDate": {
+                "newValue": "2023-06-13T11:54:07.583Z"
+            },
+            "WEF_074AE9F3108D4C65993E6BEEA1D86824_Kanban.Column": {"newValue": "New"},
+            "WEF_F10BD31DDF194FA7B15351E3F028EBD9_Kanban.Column": {"newValue": "New"},
+            "System.Title": {
+                "newValue": "[3D] Mejora Visual de arcadas: Postprocessing"
+            },
+            "System.Description": {"newValue": "TODO"},
+            "Microsoft.VSTS.Common.BacklogPriority": {"newValue": 999685268.0},
+            "WEF_D96794364361494ABA00132745C4DEB4_System.ExtensionMarker": {
+                "newValue": True
+            },
+            "WEF_27D37A568BC84A79804B57CD0E4B83D1_System.ExtensionMarker": {
+                "newValue": True
+            },
+            "WEF_F10BD31DDF194FA7B15351E3F028EBD9_System.ExtensionMarker": {
+                "newValue": True
+            },
+            "WEF_22C2BBA295614D779A28B8CD0DA94944_System.ExtensionMarker": {
+                "newValue": True
+            },
+            "System.Tags": {"newValue": "[Visor]; ToRefineEstimation"},
+            "System.Parent": {"newValue": 165687},
+        },
+        "relations": {
+            "added": [
+                {
+                    "rel": "System.LinkTypes.Related",
+                    "url": "https://dev.azure.com/plainconcepts/4b5209c5-0a90-4a6e-acef-1eaa20630758/_apis/wit/workItems/168889",
+                    "attributes": {"isLocked": False, "name": "Related"},
+                },
+                {
+                    "rel": "System.LinkTypes.Hierarchy-Reverse",
+                    "url": "https://dev.azure.com/plainconcepts/4b5209c5-0a90-4a6e-acef-1eaa20630758/_apis/wit/workItems/165687",
+                    "attributes": {"isLocked": False, "name": "Parent"},
+                },
+            ]
+        },
+        "url": "https://dev.azure.com/plainconcepts/4b5209c5-0a90-4a6e-acef-1eaa20630758/_apis/wit/workItems/168975/updates/1",
+    }
+
+
+@pytest.fixture
+def updates(update):
+    return {
+        "count": 1,
+        "value": [update],
+    }
+
+
+def assert_parse_state_update(update):
+    log = parse_state_update(update)
+    assert len(log) == 3
+    assert log == [168975, parser.parse("2023-06-13T11:57:06.947Z"), "New"]
+
+
+def assert_states_table_colums(empty_query_result):
+    df = get_wit_state_updates(empty_query_result)
     columns = df.columns.tolist()
-    assert len(columns) == 4
-    assert columns == ["Id", "Title", "State", "Work Item Type"]
+    assert len(columns) == 3
+    assert columns == ["id", "date", "state"]
